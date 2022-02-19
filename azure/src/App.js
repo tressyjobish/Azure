b60e45b9c9c8e5d838e52d1ffe5e86c44103cdcc
tressyjobish <tressyjobish@gmail.com>
2022-02-19 10:21:01 +0530

    feat: Azure website intitial commit

---
+++ azure/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      
+     <Navbar/>
+     <Banner/>
+     <Card/>
+    </div>
+  );
+}
+
+export default App;
---
+++ azure/src/components/CardData.js
@@ -0,0 +1,10 @@
+let CardData=[ 
+    {
+        imgsrc:"https://azurecomcdn.azureedge.net/cvt-78b49a463fc0…44a8b030375f4d3/images/page/home/open-source.webp"
+        h3:"Use your favorite open-source tools on Azure"
+        p:"Innovate faster and more flexibly-in any environment-using your favorite tools",<br>
+        p:"languages,and frameworks."
+    }
+
+]
+
